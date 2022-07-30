dd34d7dd6e4a273e90dc6eaaa7c4fca710f2cfa4
hemang1717 <96871477+hemang1717@users.noreply.github.com>
2022-07-30 11:08:40 +0530

    Add files via upload

---
+++ option.js
@@ -0,0 +1,22 @@
+let saveBtn = document.querySelector("#save");
+let api = document.querySelector("#myapi")
+let toastError = document.querySelector('.toast-error')
+let toastSuccess = document.querySelector('.toast-success')
+
+saveBtn.addEventListener('click', () => {
+    if (api.value) {
+        toastSuccess.classList.remove('d-hide');
+
+        // storing the data in extension
+	//api gets stored in the browser local storage not in any cloud
+        chrome.storage.local.set({ "API": api.value }, function () {
+            console.log('Value is set to ' + api.value);
+        });
+
+    } else {
+        toastError.classList.remove('d-hide')
+        setTimeout(() => {
+            toastError.classList.add('d-hide')
+        }, 1500)
+    }
+})
